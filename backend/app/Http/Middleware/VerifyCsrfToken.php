3fb96863cd87ea0cdff9fd6a50dc6c2af0833c96
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-29 07:26:51 +0900

    Add files via upload

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        '/',
         'run',
+        'run/*',
         'download/*',
+        'test',
     ];
 }

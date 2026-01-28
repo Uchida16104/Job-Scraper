f5f37732f6517f9f78dbf4fd07555bd8242c8e8c
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-29 07:27:20 +0900

    Add files via upload

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,13 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'run', 'download/*'],
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '/', 'run', 'download/*', 'test'],
 
     'allowed_methods' => ['*'],
 
@@ -10,15 +16,17 @@ return [
         env('FRONTEND_URL', 'http://localhost:3000'),
         'http://localhost:3000',
         'https://*.vercel.app',
+        'https://job-scraper-indol-nine.vercel.app',
     ],
 
     'allowed_origins_patterns' => [
         '/^https:\/\/.*\.vercel\.app$/',
+        '/^http:\/\/localhost(:\d+)?$/',
     ],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Content-Disposition'],
 
     'max_age' => 0,
 
